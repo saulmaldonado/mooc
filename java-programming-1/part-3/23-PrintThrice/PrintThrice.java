a2dab31584b2242cd3fe8708edcdf196aae5419a
Saul Maldonado <samaldonado4@gmail.com>
2021-02-21 02:05:38 -0600

    feat: Add part-3 23-PrintThrice

---
+++ java-programming-1/part-3/23-PrintThrice/PrintThrice.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class PrintThrice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        
+        String message = scanner.nextLine();
+        for(int i = 0; i < 3; i++) {
+            System.out.print(message);
+        }
+
+    }
+}
+
