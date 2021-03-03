afd3e3bcfa382c1d60af542e6227b388df2aa806
Saul Maldonado <samaldonado4@gmail.com>
2021-03-02 21:29:26 -0600

    feat: Add part-5 03-Cube

---
+++ java-programming-1/part-5/03-Cube/Cube.java
@@ -0,0 +1,17 @@
+public class Cube {
+    private final int edgeLength;
+    
+    public Cube(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+    
+    public int volume() {
+        return edgeLength * edgeLength * edgeLength;
+    }
+    
+    @Override
+    public String toString() {
+        return "The length of the edge is " + edgeLength + " and the volume " + volume();
+    }
+}
+
