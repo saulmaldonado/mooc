5001ff32411aa0373b4ecc7b67837c2cf1ca5846
Saul Maldonado <samaldonado4@gmail.com>
2021-03-12 22:09:28 -0600

    feat: Add part-8 02-AverageOfPositiveNumbers

---
+++ java-programming-2/part-8/02-AverageOfPositiveNumbers/AverageOfPositiveNumbers.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        
+        while(true) {
+            int input = scanner.nextInt();
+            
+            if(input == 0) {
+                break;
+            } else if(input > 0) {
+                count++;
+                sum += input;
+            }
+        }
+        
+        if(count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) sum / count);
+        }
+    }
+}
+
